7e539bdc13aca6994b27aef40dd3a3f2342ca0c8
qubenxuan <qubenxuan@outlook.com>
2024-03-17 00:20:53 +0800

    Changes

--- 1.py
+++ 1.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif'] = ['Songti SC']
-import numpy as np
-df = pd.read_spss('例4-22数据.sav') #读取sps
+
+df = pd.read_spss('例4-22数据.sav')  # 读取spss
 df.to_excel('例4-22数据.xlsx')
